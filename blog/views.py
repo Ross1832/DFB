171deaa606601bd3b998d236e1b803a742d49892
Ross <komarostyslav@gmail.com>
2022-03-10 01:26:40 +0200

    Chapter 5

--- blog/views.py
+++ blog/views.py
@@ -1,2 +1,12 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home_blog.html'
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
